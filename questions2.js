56cc14b629afc693ce8357621caa764baebd0aea
Danny Sperry <danny.sperry@gmail.com>
2013-07-09 15:02:14 -0700

    added fading between questions, added working back button, score is always accurate

--- questions2.js
+++ questions2.js
@@ -4,109 +4,130 @@ $(document).ready(function(){
     //  variables  //
     /////////////////
 
-    var allQuestions = [{"question":"Do all your developers love Microsoft and all of it's products?","choices":["yes","no"],"correctAnswer":1},
-                        {"question":"What Operating System does your company develop on?","choices":["Windows","OSx","Linux"],"correctAnswer":1},
-                        {"question":"What programming language do you use for the server-side?","choices":["PHP","Ruby","Perl","Python","Node.js"],"correctAnswer":1},
-                        {"question":"Would you be willing to hire a junior/entry level front end developer?","choices":["yes","no"],"correctAnswer":0},
-                        {"question":"What kind of attire do your employees wear?","choices":["Formal","Casual","Clean clothes will do"],"correctAnswer":1}
-                        ];
+    var allQuestions = [{"question":"Do all your developers love Microsoft and all of it's products?","choices":["yes","no"],"correctAnswer":"no"},
+        {"question":"What Operating System does your company develop on?","choices":["Windows","OSx","Linux"],"correctAnswer":"OSx"},
+        {"question":"What programming language do you use for the server-side?","choices":["PHP","Ruby","Perl","Python","Node.js"],"correctAnswer":"ruby"},
+        {"question":"Would you be willing to hire a junior/entry level front end developer?","choices":["yes","no"],"correctAnswer":"yes"},
+        {"question":"What kind of attire do your employees wear?","choices":["Formal","Casual","Clean clothes will do"],"correctAnswer":"casual"}
+    ];
     var qCount = 0, points = 0, length = allQuestions.length, correct = [];
 
     /////////////////
     //  functions  //
     /////////////////
 
-    var start = function() {
-    $('header').css({'display':'block'});    // display settings
-    $('#main').css({'display':'block'});     // set up for javascript
-    $('#noscript').css({'display':'none'});  // disabled users
+    var set = function() {
+        // display settings set up for javascript disabled users
+        $('header').css({'display':'block'});
+        $('#main').css({'display':'block'});
+        $('#noscript').css({'display':'none'});
+        // clear all questions within .question and create all new #question divs within .question
+        questions();
+        // hide all elements other than #intro and .question
+        $('.content').not($('#intro')).hide();
+        $('.question').children().hide();
+        $('#intro, .question').show();
 
-    $('.content').not($('#intro')).hide();
-    $('#intro').show();
-    points = 0;
-    qCount = 0;
-    correct = [];
+        // reset variables to starting values
+        qCount = 0;
+        points = 0;
+        correct = [];
     };
+
 
     String.prototype.capitalize = function() {
         return this.charAt(0).toUpperCase() + this.slice(1);
     };
 
-    var answer = function(i) {
+    var questions = function() {
+        // clear all, if any, questions within .question
+        $('.question').html('');
+        $.each(allQuestions, function(i){
+            // create html for individual questions and choices
+            var html = "<form id = question_" + (i+1) + "><p>" + allQuestions[i].question + "</p>";
+            // loop through and create radio buttons for each choice
+            for(var x = 0 ; x < allQuestions[i].choices.length; x++){
+                html += "<input id='choice" + (i+1) + "_" + (x+1) + "' type='radio' name='radio" + (i+1) + "' class='radio' value='" + allQuestions[i].choices[x] + "' >";
+                html += "<label for='choice" + (i+1) + "_" + (x+1) + "'>" + allQuestions[i].choices[x].capitalize() + "</label><br>";
+            }
+            //make a previous button only if there is a question to go back to
+            if (i > 0) {html += "<div class='previous'>Previous</div><br>";}
+            html += "<div class='next'>Next</div></form>";
+            // show questions and insert html for next individual question
+            $('.question').append(html);
+        });
+    };
 
-        var checked = $('#question_'+ i +' input:checked');
+    var answer = function(i) {
+        var checked = $('#question_'+ (i) +' input:checked');
+        // make sure answer is checked before moving to next question
         if(checked.length !== 0){
-            if ($('#question_' + i + ' input:checked').val() === allQuestions[i-1].choices[allQuestions[i-1].correctAnswer] ){
-                correct.push(1);
+            // submit a 1 or 0 into the correct array if answer is correct
+            if (checked.val().toLowerCase() === allQuestions[i-1].correctAnswer.toLowerCase() ){
+                correct[i-1] = 1;
             } else {
-                correct.push(0);
+                correct[i-1] = 0;
             }
         } else {
             return false;
         }
     };
-    var question = function(i){
-        // create html for individual questions and choices
-        var html = "<form id = question_" + i + "><p>" + allQuestions[i-1].question + "</p>";
-        // loop through and create radio buttons for each choice
-        for(var x = 0 ; x < allQuestions[i-1].choices.length; x++){
-            html += "<input id='choice" + (i) + "_" + (x+1) + "' type='radio' name='radio" + (i) + "' class='radio' value='" + allQuestions[i-1].choices[x] + "' >";
-            html += "<label for='choice" + (i) + "_" + (x+1) + "'>" + allQuestions[i-1].choices[x].capitalize() + "</label><br>";
-        }
-        //make a previous button only if there is a question to go back to
-        if (qCount > 1) {html += "<button class='previous'>Previous</button><br>";}
-        html += "<button class='next'>Next</button></form>";
-        // show questions, clear old question html inside it, and insert html for next individual question
-        $('.question').show().empty().append(html);
-
-        $('.next').click(function(e) {
-            e.preventDefault();
-            answer(i);
-            next();
-        });
-        $('.previous').click(function(e) {
-            e.preventDefault();
-            previous();
-        });
-    };
 
 
-    // get percentage of correct answers divided by total questions
     var score = function() {
+        // add all the elements within correct array
         $.each(correct,function() {
             points += this;
         });
-        points /= length;
-        points *= 100;
+        // get percentage of correct answers divided by total questions
+        points = (points/length)*100;
         return points;
     };
 
+    var begin = function() {
+        $('#intro').fadeOut('slow',function(){
+            $('#question_1').fadeIn('slow');
+        });
+        qCount += 1;
+    };
 
-    var results = function() {
-        // stop showing questions and show results with score()
-        if (qCount-1 ===  length) {
+    // fade out last question and display score results within #score div
+    var end = function(i) {
+        $('#question_' + (i)).fadeOut(function(){
             $('.content').not($('#score')).hide();
             $('#score').show();
             $('#final').empty().append(score() + '%');
-        }
+            $('#score').fadeIn();
+        });
     };
 
-    var next = function() {
-        if (qCount === 0) {
-            qCount += 1;
-            question(qCount);
-        } else if ((qCount > 0) && (answer(qCount) !== false)) {
-            qCount += 1;
-            results();
-            question(qCount);
-        }
-
+    // fade out currect question and fade in next question
+    var forward = function(i) {
+        $('#question_' + (i)).fadeOut(function(){
+            $('#question_' + (i+1)).fadeIn();
+        });
+        qCount += 1;
     };
 
-    var previous = function() {
+    // fade out currect question and fade in previous question
+    var backward = function(i){
+        $('#question_' + (i)).fadeOut(function(){
+            $('#question_' + (i-1)).fadeIn();
+        });
         qCount -= 1;
-        question(qCount);
-        correct.pop();
+    };
+
+    var next = function(i) {
+        if(i === 0) {
+                begin();
+        } else if (answer(i) !== false){
+            if(i === (length)){
+                end(i);
+            } else {
+                forward(i);
+            }
+        }
+
     };
 
 
@@ -114,17 +135,24 @@ $(document).ready(function(){
     //  main  //
     ////////////
 
-    start();
+    set();
 
-    $('.submit').click(function(){
-        $('#intro').hide();
-        next();
+    $('.start').click(function(){
+        next(qCount);
     });
 
     $('#retry').click(function(){
-        start();
+        set();
     });
 
+    $('.question').on("click",".next",function() {
+        next(qCount);
+        answer(qCount);
+    });
+    $('.question').on("click",".previous",function() {
+        backward(qCount);
+        answer(qCount);
+    });
 
 
 });
